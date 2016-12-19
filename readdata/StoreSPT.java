41ab10ff721159c0e9bef79241982aa8f0f6ad5a
JonathanGillies <14001829@uhi.ac.uk>
2016-12-19 12:51:29 +0000

    Attempts to insert correct SPT values into database table

--- readdata/StoreSPT.java
+++ readdata/StoreSPT.java
@@ -12,22 +12,29 @@ public class StoreSPT{
  
   public StoreSPT(){
     try {
-   System.out.println("Loading JDBC driver...");
-   Class.forName("com.mysql.jdbc.Driver");
-   System.out.println("JDBC driver successfully loaded!");
-   connection = DriverManager.getConnection(url, username, password);
-   //query = "insert into spt values("")";
-   
-  } catch (ClassNotFoundException e) {
-   throw new RuntimeException(e);
-  } catch (SQLException e) {
-   System.out.println(e.toString());
+      System.out.println("Loading JDBC driver...");
+      Class.forName("com.mysql.jdbc.Driver");
+      System.out.println("JDBC driver successfully loaded!");
+      connection = DriverManager.getConnection(url, username, password);
+      //query = "insert into spt values("")";
+  
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    } catch (SQLException e) {
+      System.out.println(e.toString());
+    }
   }
   
-  
-  
-  
-  
+  public void writeRecord(DataSPT dataSPT){
+    try {
+      stmt = connection.createStatement();
+      System.out.println(dataSPT.datetime);
+      String sql = "INSERT INTO spt (date_time, Hs, Tz, Battery)" +
+        "VALUES (" + dataSPT.datetime + ", "  + dataSPT.hs + ", " + dataSPT.tz + ", " + dataSPT.battery + ")";
+      stmt.executeUpdate(sql);
+    } catch (SQLException e) {
+      System.out.println(e.toString());
+    }
   }
-  
+
 }
