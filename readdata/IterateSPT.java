f1e7657ad174f30efc2f1f302d6eebd2baf1fcc8
JonathanGillies <14001829@uhi.ac.uk>
2016-12-19 12:47:53 +0000

    Now calls writeRecord and passes Path to DataSPT constructor

--- readdata/IterateSPT.java
+++ readdata/IterateSPT.java
@@ -7,7 +7,7 @@ import java.io.*;
 public class IterateSPT {
   
   public static void main(String[] args) throws IOException {
-    String directory_path = "E:\\Documents\\Object Oriented Programming\\James Morrison\\Example file read\\spt_files";
+    String directory_path = "E:\\Documents\\Object Oriented Programming - H171 35\\spt_files_folder\\spt_files";
     
     File folder = new File(directory_path);
     File[] listOfFiles = folder.listFiles();
@@ -19,8 +19,9 @@ public class IterateSPT {
         //System.out.println(file.getPath());
         ReadSPT spt_data = new ReadSPT(file.getPath());
         String[] file_data = spt_data.OpenFile();
-        DataSPT dataSPT = new DataSPT(file_data);
-        System.out.println(dataSPT.Battery);
+        DataSPT dataSPT = new DataSPT(file_data, file.getPath());
+        storeSPT.writeRecord(dataSPT);
+        System.out.println(dataSPT.battery);
       } 
       
     };
